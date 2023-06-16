5c0ab9404bb3cc45e83a7330f6aaa5d5173ac2e1
Ayesha <ayesha18mbstu@gmail.com>
2023-06-17 04:23:18 +0600

    admin role & delete apis added

--- index.js
+++ index.js
@@ -49,6 +49,28 @@ async function run() {
       res.send(result)
     })
 
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    app.delete('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
 
     app.get("/instructors", async (req, res) => {
       const result = await instructorCollection.find().toArray();
