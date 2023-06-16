86bc08c9b341a329a27e3fe8d0950624d7bebdd6
Ayesha <ayesha18mbstu@gmail.com>
2023-06-16 06:01:21 +0600

    delete data

--- index.js
+++ index.js
@@ -71,6 +71,13 @@ async function run() {
       console.log(item);
       const result = await cartCollection.insertOne(item);
       res.send(result);
+    });
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
